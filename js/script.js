ede72d64b53a76d053353a2bf776d9721ff76cb3
usvx <d@ko.vc>
2024-08-28 18:52:25 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('#login-form button');
     const sphere = document.querySelector('.sphere');
+    const body = document.body;
+
+    let hasMouse = false;
+
+    window.addEventListener('mousemove', (e) => {
+        if (!hasMouse) {
+            hasMouse = true;
+            body.classList.add('has-mouse');
+        }
+
+        const xPercent = e.clientX / window.innerWidth * 100;
+        const yPercent = e.clientY / window.innerHeight * 100;
+        
+        body.style.background = `radial-gradient(circle at ${xPercent}% ${yPercent}%, var(--primary-bg-color), var(--secondary-bg-color) 50%, var(--tertiary-bg-color) 100%)`;
+    });
+
+    window.addEventListener('touchstart', () => {
+        if (hasMouse) {
+            hasMouse = false;
+            body.classList.remove('has-mouse');
+            body.style.background = `linear-gradient(135deg, var(--primary-bg-color) 0%, var(--secondary-bg-color) 50%, var(--tertiary-bg-color) 100%)`;
+        }
+    });
 
     button.addEventListener('mouseenter', () => {
         sphere.style.background = 'radial-gradient(circle at 50% 50%, var(--button-hover-bg-start), var(--button-hover-bg-end) 70%)';

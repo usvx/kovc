50bf988be4117713fda2f4af2b46b5f7b6b309b0
usvx <d@ko.vc>
2024-10-26 11:30:52 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // js/script.js
 
 // Import Three.js from Skypack as an ES Module
-import * as THREE from 'https://cdn.skypack.dev/three@0.152.2/build/three.module.js';
+import * as THREE from 'https://cdn.skypack.dev/three@0.152.2';
 
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('login-form'),
